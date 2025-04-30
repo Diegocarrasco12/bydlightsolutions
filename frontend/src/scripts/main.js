066ca178987678929fc7990a89d8d29ac0cd0032
Diego_Carrasco <diegocarrasco.ordonez@gmail.com>
2025-04-29 20:33:05 -0400

    mejoras responsive

---
+++ frontend/src/scripts/main.js
@@ -0,0 +1,7 @@
+const mostrarFormulario = () => {
+    const formulario = document.getElementById('formularioContacto');
+    formulario.style.display = 'block';
+    formulario.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  
